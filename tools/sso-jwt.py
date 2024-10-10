58804313fa0f27ee6750d0e6a705504f30234c09
Pavlo Suslykov <Pavlo Suslykov>
2024-10-10 15:17:55 -0700

    feature: sso-jwt added environment argument

--- tools/sso-jwt.py
+++ tools/sso-jwt.py
@@ -1,6 +1,6 @@
-from gd_auth.client import AwsIamAuthTokenClient
+import argparse
 
-ENV = "prod"
+from gd_auth.client import AwsIamAuthTokenClient
 
 conf = {
     "dev": {
@@ -13,10 +13,20 @@ conf = {
     }
 }
 
-sso_client = AwsIamAuthTokenClient(
-    conf[ENV]["sso_host"],
-    refresh_min=45,
-    primary_region='us-west-2',
-    secondary_region='us-west-2')
-iam_token = sso_client.token
-print(iam_token)
+
+def generate_iam_token(env: str):
+    sso_client = AwsIamAuthTokenClient(
+        conf[env]["sso_host"],
+        refresh_min=45,
+        primary_region='us-west-2',
+        secondary_region='us-west-2')
+    return sso_client.token
+
+
+if __name__ == '__main__':
+    argument_parser = argparse.ArgumentParser("Generate IAM token for SSO")
+    argument_parser.add_argument("-e", "--env", type=str, required=True, help="Environment to generate IAM token for", choices=["dev", "prod"])
+
+    args = argument_parser.parse_args()
+
+    print(generate_iam_token(env=args.env))
